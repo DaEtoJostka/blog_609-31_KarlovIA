aaf99294d235424806fc01e2617be678d7e6fa80
DaEtoJostka <karlovivan679@gmail.com>
2025-04-28 12:16:52 +0500

    Remove DTL.md and implement user authentication features. Added thumbnail field to Article model, updated article_item.html to display thumbnails, and modified base_layout.html for login/logout functionality. Updated settings.py for media file handling.

--- mysite4/mysite4/settings.py
+++ mysite4/mysite4/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'articles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# Media files (Uploaded by users)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
