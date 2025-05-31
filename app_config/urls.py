e1c485919a2cbf63dd59f5ef12846cd3af9d8279
DaEtoJostka <karlovivan679@gmail.com>
2025-05-31 16:00:29 +0500

    Update database configuration to use MySQL and set static file directory in settings.py

--- app_config/urls.py
+++ app_config/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('accounts/', include('apps.accounts.urls')),
 ]
 
-# Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
